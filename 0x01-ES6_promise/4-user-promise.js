f518e5d6d2f83bed7a5f101fa32e78e2ae0475a3
Ademileke12 <ademilekeabudu@gmail.com>
2023-03-22 19:04:43 +0000

    4-user-promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,11 @@
+// Sign up user
 function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-      firstName: firstName,
-      lastName: lastName,
-    });
+  return Promise.resolve(
+    {
+      firstName,
+      lastName,
+    },
+  );
 }
+
 export default signUpUser;
