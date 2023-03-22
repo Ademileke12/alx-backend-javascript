e68a703a980d5b118c09e61ffc19266789d56b9a
Ademileke12 <ademilekeabudu@gmail.com>
2023-03-22 13:17:07 +0000

    0-promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+//keep every promise you can make and only make promise you can keep
+function getResponseFromAPI(){
+	var promise = new Promise((resolve, reject){
+		if(true){
+			resolve();
+		}
+		else{
+			reject();
+		}
+	});
+}
